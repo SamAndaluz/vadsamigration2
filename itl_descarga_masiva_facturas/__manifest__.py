97bf1e1e62d4165b88921731234673df6133528f
A. Márquez <adan@itlglobal.tech>
2021-10-12 22:39:02 +0000

    updates

--- itl_descarga_masiva_facturas/__manifest__.py
+++ itl_descarga_masiva_facturas/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Account',
-    'version': '0.1',
+    'version': '14.0.1.0.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base','mail','account','stock','l10n_mx_edi'],
